54901856e24497d0262ee8c3526805d28ac64513
Andreas Bøgh Poulsen <andreas@lha66.dk>
2014-04-01 17:47:16 +0200

    Added abiword and gnumeric to remove list

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 PKGS="wajig gedit libreoffice chromium-browser gimp openshot scribus openssh-server vim"
+TO_REMOVE="abiword gnumeric apport"
 
 sudo apt-get -y install $PKGS
-sudo apt-get -y remove apport
+sudo apt-get -y remove $TO_REMOVE
 
 cp autostart /etc/skel/.config/lxsession/Lubuntu/
 cp -b lightdm.conf /etc/lightdm/lightdm.conf
