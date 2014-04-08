1fa81c7040a464b5edbcca48a87df16472a418b3
Andreas <0708andreas@gmail.com>
2014-04-08 15:47:31 +0200

    Update install.sh
    Changed to avoid depending on /etc/skel

--- install.sh
+++ install.sh
@@ -4,10 +4,12 @@ TO_REMOVE="abiword gnumeric apport update-manager update-manager-core"
 sudo apt-get -y install $PKGS
 sudo apt-get -y remove $TO_REMOVE
 
-cp autostart /etc/skel/.config/lxsession/Lubuntu/
+#cp autostart /etc/skel/.config/lxsession/Lubuntu/
+cp autostart /home/elev_skel/.config/lxsession/Lubuntu/
+cp autostart /home/elev/.config/lxsession/Lubuntu/
 cp -b lightdm.conf /etc/lightdm/lightdm.conf
 
-./create_users.sh
+#./create_users.sh
 
 cp -r website/ /home/egeadmin
 ln -s /home/egeadmin/website/index.html /home/elev_skel/Skrivebord/intro.html
