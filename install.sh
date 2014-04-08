1b48fc523a7077ee508ba14a44d3ac0f2448aba7
egeadmin <0708andreas@gmail.com>
2014-04-08 11:35:38 +0200

    Added update-manager to remove list

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 PKGS="wajig gedit libreoffice chromium-browser gimp openshot scribus openssh-server vim"
-TO_REMOVE="abiword gnumeric apport"
+TO_REMOVE="abiword gnumeric apport update-manager update-manager-core"
 
 sudo apt-get -y install $PKGS
 sudo apt-get -y remove $TO_REMOVE
@@ -8,3 +8,7 @@ cp autostart /etc/skel/.config/lxsession/Lubuntu/
 cp -b lightdm.conf /etc/lightdm/lightdm.conf
 
 ./create_users.sh
+
+
+
+
